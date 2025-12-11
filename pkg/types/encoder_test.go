341e22f2606020a8ae8be847e382d4ae8a835a03
Eric Promislow <epromislow@suse.com>
2025-12-11 14:12:26 -0800

    Add a summary field to the APIObjectList - #51337 (#210)
    * Add a summary field to the APIObjectList - #51337
    
    * Add summary and tests to pkg/types/encoder_test.go

--- pkg/types/encoder_test.go
+++ pkg/types/encoder_test.go
@@ -31,13 +31,33 @@ func TestJSONLinesEncoder(t *testing.T) {
 	}{
 		{
 			name:       "empty collection list",
-			args:       args{&types.GenericCollection{collection, []*types.RawResource{}}},
+			args:       args{&types.GenericCollection{collection, []*types.RawResource{}, []types.SummaryEntry{}}},
 			wantWriter: "{\"links\":{},\"actions\":{},\"resourceType\":\"Test\"}\n\n",
 		},
 		{
 			name:       "valid collection list",
-			args:       args{&types.GenericCollection{collection, data}},
+			args:       args{&types.GenericCollection{collection, data, []types.SummaryEntry{}}},
 			wantWriter: "{\"links\":{},\"actions\":{},\"resourceType\":\"Test\"}\n{\"links\":null}\n{\"links\":null}\n{\"links\":null}\n\n",
+		},
+		{
+			name: "valid collection list with summary",
+			args: args{&types.GenericCollection{Collection: collection, Data: data, Summary: []types.SummaryEntry{
+				types.SummaryEntry{
+					Property: "field01",
+					Counts:   map[string]int{"walrus": 3, "cat": 4},
+				},
+				types.SummaryEntry{
+					Property: "field02",
+					Counts:   map[string]int{"walrus": 5, "cat": 2},
+				}}}},
+			wantWriter: `{"links":{},"actions":{},"resourceType":"Test"}
+{"links":null}
+{"links":null}
+{"links":null}
+{"property":"field01","counts":{"cat":4,"walrus":3}}
+{"property":"field02","counts":{"cat":2,"walrus":5}}
+
+`,
 		},
 		{
 			name:       "arbitrary type",
