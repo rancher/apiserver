97f13983f136c44396390f6f06ed67a8ff1cee37
Eric Promislow <epromislow@suse.com>
2026-04-27 16:06:59 -0700

    Add unit tests for the url-parser.

--- pkg/parse/mux_test.go
+++ pkg/parse/mux_test.go
@@ -9,9 +9,9 @@ import (
 )
 
 func TestStandardURLParserUsesPathValuesForActionAndLink(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/v1/foos/one?action=queryAction&link=queryLink", nil)
-	req.SetPathValue("type", "foos")
-	req.SetPathValue("name", "one")
+	req := httptest.NewRequest(http.MethodGet, "/v1/resource/objectName?action=queryAction&link=queryLink", nil)
+	req.SetPathValue("type", "resource")
+	req.SetPathValue("name", "objectName")
 	req.SetPathValue("action", "pathAction")
 	req.SetPathValue("link", "pathLink")
 
@@ -19,14 +19,14 @@ func TestStandardURLParserUsesPathValuesForActionAndLink(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, "pathAction", parsed.Action)
 	require.Equal(t, "pathLink", parsed.Link)
-	require.Equal(t, "foos", parsed.Type)
-	require.Equal(t, "one", parsed.Name)
+	require.Equal(t, "resource", parsed.Type)
+	require.Equal(t, "objectName", parsed.Name)
 }
 
 func TestStandardURLParserFallsBackToQueryWhenPathValuesMissing(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "/v1/foos/one?action=queryAction&link=queryLink", nil)
-	req.SetPathValue("type", "foos")
-	req.SetPathValue("name", "one")
+	req := httptest.NewRequest(http.MethodGet, "/v1/resource/objectName?action=queryAction&link=queryLink", nil)
+	req.SetPathValue("type", "resource")
+	req.SetPathValue("name", "objectName")
 
 	parsed, err := StandardURLParser(httptest.NewRecorder(), req, nil)
 	require.NoError(t, err)
