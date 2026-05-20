bdcf5579079dfcee9eb920263d379f31366c2f71
Eric Promislow <epromislow@suse.com>
2026-05-20 14:52:14 -0700

    copilot suggested testing this.

--- pkg/types/encoder_test.go
+++ pkg/types/encoder_test.go
@@ -45,23 +45,23 @@ func TestJSONLinesEncoder(t *testing.T) {
 				types.SummaryEntry{
 					Property: "field01",
 					Counts: map[string]types.SummaryWithBreakdown{
-						"walrus": {Total: 3},
-						"cat":    {Total: 4},
+						"walrus": {Total: 3, Namespace: map[string]int{"zoo": 1, "park": 2}},
+						"cat":    {Total: 4, Namespace: map[string]int{"house": 3, "nursing home": 1}},
 					},
 				},
 				types.SummaryEntry{
 					Property: "field02",
 					Counts: map[string]types.SummaryWithBreakdown{
-						"walrus": {Total: 5},
-						"cat":    {Total: 2},
+						"walrus": {Total: 5, Namespace: map[string]int{"zoo": 3, "park": 2}},
+						"cat":    {Total: 2, Namespace: map[string]int{"house": 1, "nursing home": 1}},
 					},
 				}}}},
 			wantWriter: `{"links":{},"actions":{},"resourceType":"Test"}
 {"links":null}
 {"links":null}
 {"links":null}
-{"property":"field01","counts":{"cat":{"total":4},"walrus":{"total":3}}}
-{"property":"field02","counts":{"cat":{"total":2},"walrus":{"total":5}}}
+{"property":"field01","counts":{"cat":{"total":4,"namespace":{"house":3,"nursing home":1}},"walrus":{"total":3,"namespace":{"park":2,"zoo":1}}}}
+{"property":"field02","counts":{"cat":{"total":2,"namespace":{"house":1,"nursing home":1}},"walrus":{"total":5,"namespace":{"park":2,"zoo":3}}}}
 
 `,
 		},
